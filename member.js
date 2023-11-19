363892881c9c1cc6f74571c7e32a2d5b6a4389c9
GogoRit <143546221+GogoRit@users.noreply.github.com>
2023-11-19 00:43:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMember = document.getElementById("skillsMember");
+    if (member.checked == true) {
+        skillsMember.style.display = "block";
+        skills.style.display = "none";
+        skillsMember.style.display = "none";
+        skillsMember.style.display = "none";
+        skillsMember.style.display = "none";
+    } else {
+        skillsMember.style.display = "none";
+        skills.style.display = "block";
+        skillsMember.style.display = "block";
+        skillsMember.style.display = "block";
+        skillsMember.style.display = "block";
+    }
+}
